0c3e66cbaeee5f7ba1ba0939a81177878c3e96a5
masterIsuru <master.isuru.595@gmail.com>
2016-06-21 22:36:13 +0530

    Add news controllers, pages added

--- js/main.js
+++ js/main.js
@@ -10,21 +10,6 @@ var app = angular.module('tutorialWebApp', [
   'ngRoute','uiSwitch','colorpicker.module'
 ]);
 
-app.directive('fileModel', ['$parse', function ($parse) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var model = $parse(attrs.fileModel);
-            var modelSetter = model.assign;
-
-            element.bind('change', function(){
-                scope.$apply(function(){
-                    modelSetter(scope, element[0].files[0]);
-                });
-            });
-        }
-    };
-}]);
 /**
  * Configure the Routes
  */
